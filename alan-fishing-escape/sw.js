16df19dd464b00d86a33bf05244bb472df5a2e90
2strongtrainers-art <2strongtrainers@gmail.com>
2026-09-05 09:51:39 -0700

    Add Fishing Escape offline cache

---
+++ alan-fishing-escape/sw.js
@@ -0,0 +1,5 @@
+const CACHE='alan-fishing-escape-v3';
+const CORE=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res}).catch(()=>hit)))})
